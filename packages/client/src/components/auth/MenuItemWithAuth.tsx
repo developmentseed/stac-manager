a7c06c96e02c2eb8e308d98b3474d5af9a79375a
Anthony Lukach <anthonylukach@gmail.com>
2026-04-23 11:55:07 -0700

    refactor(auth): use useAuth in ButtonWithAuth/MenuItemWithAuth

--- packages/client/src/components/auth/MenuItemWithAuth.tsx
+++ packages/client/src/components/auth/MenuItemWithAuth.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { forwardRef, MenuItem, MenuItemProps } from '@chakra-ui/react';
-import { useKeycloak } from 'src/auth/Context';
+import { useAuth } from 'src/auth/Context';
 
 export const MenuItemWithAuth = forwardRef<MenuItemProps, typeof MenuItem>(
   (props, ref) => {
-    const { isEnabled, keycloak } = useKeycloak();
+    const { isEnabled, isAuthenticated } = useAuth();
 
     if (!isEnabled) {
       return <MenuItem ref={ref} {...props} />;
     }
 
-    return keycloak.authenticated && <MenuItem ref={ref} {...props} />;
+    return isAuthenticated && <MenuItem ref={ref} {...props} />;
   }
 );
